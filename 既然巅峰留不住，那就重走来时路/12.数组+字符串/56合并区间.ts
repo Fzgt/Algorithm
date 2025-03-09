c812e71603bd350744e7028627984d068f26dad1
Fzgt <62455458+Fzgt@users.noreply.github.com>
2025-03-09 13:35:12 +1100

    合并区间

---
+++ 既然巅峰留不住，那就重走来时路/12.数组+字符串/56合并区间.ts
@@ -0,0 +1,24 @@
+// 56. 合并区间
+function merge(intervals: number[][]): number[][] {
+    if (intervals.length === 0) return [];
+
+    const res = [];
+
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    res.push(intervals[0]);
+
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] > res[res.length - 1][1]) { // 有交集
+            res.push(intervals[i]);
+        } else {
+            // 无交集, 但也需要保证当前元素的右端点大于res中最后一个元素的右端点
+            // 因为可能有[[1, 4], [2, 3]]这种情况
+            if (intervals[i][1] > res[res.length - 1][1]) {
+                res[res.length - 1][1] = intervals[i][1];
+            }
+        }
+    }
+
+    return res;
+};
