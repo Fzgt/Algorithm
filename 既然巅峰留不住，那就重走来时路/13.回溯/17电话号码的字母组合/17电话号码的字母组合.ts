ec421a5cbd99b179bd5a65346a96b7001148bc53
Fzgt <62455458+Fzgt@users.noreply.github.com>
2025-04-15 10:39:17 +1000

    整理 回溯文件夹目录

--- 既然巅峰留不住，那就重走来时路/13.回溯/17电话号码的字母组合/17电话号码的字母组合.ts
+++ 既然巅峰留不住，那就重走来时路/13.回溯/17电话号码的字母组合/17电话号码的字母组合.ts
@@ -5,11 +5,11 @@ function letterCombinations(digits: string): string[] {
     const map = { '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz' };
 
     const dfs = (path, i) => {
-        if (i > digits.length - 1) {
+        if (path.length > digits.length - 1) {
             ans.push(path);
             return;
         }
-        const letters = map[digits[i]];
+        const letters = map[digits[i]]; // 取新的字母对应的字符串
         for (const letter of letters) {
             dfs(path + letter, i + 1); // 不用回溯 因为每个dfs都是新字符串，值传递
         }
@@ -18,3 +18,9 @@ function letterCombinations(digits: string): string[] {
     dfs('', 0);
     return ans;
 };
+
+// 如何 '23' 的情况
+// 两层递归就够了
+// 第一层 选出来 a b c
+// 第二层 用选出来的 a b c 去拼接 3 的字母 d e f
+// 3 乘 3 得到九种结果，path.length 正好也等于2 结束递归
