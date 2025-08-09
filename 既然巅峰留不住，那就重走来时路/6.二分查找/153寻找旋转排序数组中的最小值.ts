4cafded34cdaa5b64cde9692310fa71d7a4a05c9
Fzgt <62455458+Fzgt@users.noreply.github.com>
2025-08-09 23:19:25 +1000

    update

--- 既然巅峰留不住，那就重走来时路/6.二分查找/153寻找旋转排序数组中的最小值.ts
+++ 既然巅峰留不住，那就重走来时路/6.二分查找/153寻找旋转排序数组中的最小值.ts
@@ -2,9 +2,9 @@ function findMin(nums: number[]): number {
     let l = 0, r = nums.length - 1;
     while (l < r) {
         let mid = l + ((r - l) >> 1);
-        if (nums[mid] < nums[r]) {
+        if (nums[mid] < nums[r]) { // 没取到mid，mid未知的，所以r可以直接取mid
             r = mid;
-        } else {
+        } else { // 这里else相当于 nums[mid] >= nums[r], 那说明最小值在右侧，l可以直接取mid+1
             l = mid + 1;
         }
     }
--- 既然巅峰留不住，那就重走来时路/6.二分查找/35搜索插入位置.ts
+++ 既然巅峰留不住，那就重走来时路/6.二分查找/35搜索插入位置.ts
@@ -4,11 +4,11 @@ function searchInsert(nums: number[], target: number): number {
     let l = 0, r = n - 1, ans = n;
     while (l <= r) {
         let mid = ((r - l) >> 1) + l;
-        if (nums[mid] >= target) {
-            ans = mid;
-            r = mid - 1;
+        if (nums[mid] >= target) { // >= target, 属于把target逼到左侧，暗示要往左侧插
+            ans = mid; // 记录可能的插入位置
+            r = mid - 1; // 找左侧边界
         }
-        else l = mid + 1;
+        else l = mid + 1; // 往右侧部分查找
     }
 
     return ans;
